b0f3ad30d97d66c1661290ebf19b6f2992eeaf5f
chanchimetro <chanchimetro@gmail.com>
2025-10-06 17:41:06 -0300

    added taller3

---
+++ Taller3 /src/main/java/aed/ListaEnlazada.java
@@ -0,0 +1,72 @@
+package aed;
+
+public class ListaEnlazada<T> {
+    // Completar atributos privados
+
+    private class Nodo {
+        // Completar
+    }
+
+    public ListaEnlazada() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public int longitud() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void agregarAdelante(T elem) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void agregarAtras(T elem) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public T obtener(int i) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void eliminar(int i) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void modificarPosicion(int indice, T elem) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public ListaEnlazada(ListaEnlazada<T> lista) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+    
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public class ListaIterador{
+    	// Completar atributos privados
+
+        public boolean haySiguiente() {
+	        throw new UnsupportedOperationException("No implementada aun");
+        }
+        
+        public boolean hayAnterior() {
+	        throw new UnsupportedOperationException("No implementada aun");
+        }
+
+        public T siguiente() {
+	        throw new UnsupportedOperationException("No implementada aun");
+        }
+        
+
+        public T anterior() {
+	        throw new UnsupportedOperationException("No implementada aun");
+        }
+    }
+
+    public ListaIterador iterador() {
+	    throw new UnsupportedOperationException("No implementada aun");
+    }
+
+}
