f5ae2b42ba0b481c1655efcece592eafaea9ccf6
Nacho <48773442+chanchimetro@users.noreply.github.com>
2025-10-17 13:15:28 -0300

    Add files via upload

---
+++ taller4/src/main/java/aed/ABB.java
@@ -0,0 +1,173 @@
+package aed;
+
+// Todos los tipos de datos "Comparables" tienen el método compareTo()
+// elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
+public class ABB<T extends Comparable<T>> {
+    private Nodo _raiz;
+
+
+    private class Nodo {
+        T _valor;
+        Nodo _der;
+        Nodo _izq;
+        Nodo _padre;
+
+        Nodo (T valor) {
+            _valor = valor;
+            _der = null;
+            _izq = null;
+            _padre = null;
+        }
+    }
+
+    public ABB() {
+        _raiz = null;
+    }
+
+    public int cardinal() {
+        int cardinal = 0;
+        Nodo actual = _raiz;
+        
+        if (actual != null) {
+            cardinal++;
+            if (actual._der != null) {
+                actual = actual._der;
+                cardinal += cardinalRecursivo(actual);
+            }
+            if (actual._izq != null) {
+                actual = actual._izq;
+                cardinal += cardinalRecursivo(actual);
+            }
+        }
+
+        return cardinal;
+    }
+
+    private int cardinalRecursivo(Nodo actual){
+        int ret = 0;
+
+        if (actual != null) {
+            ret++;
+            if (actual._der != null) {
+                actual = actual._der;
+                ret += cardinalRecursivo(actual);
+            }
+            if (actual._izq != null) {
+                actual = actual._izq;
+                ret += cardinalRecursivo(actual);
+            }
+        }
+
+        return ret;
+    }
+
+    public T minimo(){
+        Nodo actual = _raiz;
+        T min = actual._valor;
+
+        while (actual._izq != null) {
+            actual = actual._izq; 
+            min = actual._valor;
+        }
+
+        return min;
+    }
+
+    public T maximo(){
+        Nodo actual = _raiz;
+        T max = actual._valor;
+
+        while (actual._der != null) {
+            actual = actual._der; 
+            max = actual._valor;
+        }
+
+        return max;
+    }
+
+    public void insertar(T elem){
+        Nodo actual = _raiz;
+        Nodo padreActual = null;
+        Nodo nuevo = new Nodo(elem);
+
+        while (actual != null) {
+           if (elem.compareTo(actual._valor) > 0) {
+            padreActual = actual;
+            actual = actual._der;
+           } else {
+            padreActual = actual;
+            actual = actual._izq;
+           }
+        }
+
+        if (actual == _raiz) {
+            _raiz = nuevo;
+        } else {
+            actual = nuevo;
+            actual._padre = padreActual;
+            
+        }
+    }
+
+    public boolean pertenece(T elem){
+        boolean pertenece = false;
+        Nodo actual = _raiz;
+
+        if (actual != null) {
+            if (actual._valor == elem) {
+                pertenece = true;
+            } else {
+                if (elem.compareTo(actual._valor) > 0) {
+                    pertenece = perteneceRecursivo(actual._der, elem);
+                } else {
+                    pertenece = perteneceRecursivo(actual._izq, elem);
+                }
+            }
+        }
+
+        return pertenece;
+    }
+
+    private boolean perteneceRecursivo(Nodo actual, T elem) {
+        boolean ret = false;
+
+        if (actual != null) {
+            if (actual._valor == elem) {
+                ret = true;
+            } else {
+                if (elem.compareTo(actual._valor) > 0) {
+                    ret = perteneceRecursivo(actual._der, elem);
+                } else {
+                    ret = perteneceRecursivo(actual._izq, elem);
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public void eliminar(T elem){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public String toString(){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public class ABB_Iterador {
+        private Nodo _actual;
+
+        public boolean haySiguiente() {            
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    
+        public T siguiente() {
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    }
+
+    public ABB_Iterador iterador() {
+        return new ABB_Iterador();
+    }
+
+}
