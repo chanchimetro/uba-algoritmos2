cef3460cba9cb3c1e75c7abf8cbe6ad6d0217170
chanchimetro <chanchimetro@gmail.com>
2025-10-25 16:07:39 -0300

    eliminar avanzado (pendiente)

--- taller4/src/main/java/aed/ABB.java
+++ taller4/src/main/java/aed/ABB.java
@@ -31,12 +31,10 @@ public class ABB<T extends Comparable<T>> {
         if (actual != null) {
             cardinal++;
             if (actual._der != null) {
-                actual = actual._der;
-                cardinal += cardinalRecursivo(actual);
+                cardinal += cardinalRecursivo(actual._der);
             }
             if (actual._izq != null) {
-                actual = actual._izq;
-                cardinal += cardinalRecursivo(actual);
+                cardinal += cardinalRecursivo(actual._izq);
             }
         }
 
@@ -49,12 +47,10 @@ public class ABB<T extends Comparable<T>> {
         if (actual != null) {
             ret++;
             if (actual._der != null) {
-                actual = actual._der;
-                ret += cardinalRecursivo(actual);
+                ret += cardinalRecursivo(actual._der);
             }
             if (actual._izq != null) {
-                actual = actual._izq;
-                ret += cardinalRecursivo(actual);
+                ret += cardinalRecursivo(actual._izq);
             }
         }
 
@@ -105,7 +101,11 @@ public class ABB<T extends Comparable<T>> {
         } else {
             actual = nuevo;
             actual._padre = padreActual;
-            
+            if (elem.compareTo(padreActual._valor) > 0) {
+                padreActual._der = nuevo;
+            } else {
+                padreActual._izq = nuevo;
+            }
         }
     }
 
@@ -147,7 +147,54 @@ public class ABB<T extends Comparable<T>> {
     }
 
     public void eliminar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo actual = _raiz;
+
+        //Encuentro el nodo que quiero eliminar.
+        while(actual._valor != elem) {
+            if (elem.compareTo(actual._valor) > 0) {
+                actual = actual._der;
+            } else {
+                actual = actual._izq;
+            }
+        }
+
+        //separo en casos!
+        if(actual._der == null && actual._izq == null) {
+            if (actual._padre != null) {
+                actual._padre = null;
+            }
+            actual = null;
+        } else if (actual._der != null && actual._izq == null) {
+            if (actual._padre != null) {
+                actual._padre._der = actual._der;
+            }
+            actual = actual._der;
+        } else if (actual._izq != null && actual._der == null) {
+            if (actual._padre != null) {
+                actual._padre._izq = actual._izq;
+            }
+            actual = actual._izq;
+        } else {
+            T pred = predecesorInmediato(actual);
+            eliminar(pred);
+            if (actual._padre != null) {
+                if (elem.compareTo(actual._padre._valor) > 0) {
+                    actual._padre._der._valor = pred;
+                } else {
+                    actual._padre._izq._valor = pred;
+                }
+            }
+            actual._valor = pred;
+        }
+    }
+
+    private T predecesorInmediato(Nodo actual) {
+        Nodo predecesor = actual._der;
+        while (predecesor._izq != null) {
+            predecesor = predecesor._izq;
+        }
+
+        return predecesor._valor;
     }
 
     public String toString(){
