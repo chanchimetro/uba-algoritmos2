602c8a4febd1183a5ddbfd1b5a022bb5d670a8c8
chanchimetro <chanchimetro@gmail.com>
2025-10-28 22:29:20 -0300

    agregue taller5

---
+++ taller5/src/main/java/aed/ABB.java
@@ -0,0 +1,48 @@
+package aed;
+
+public class ABB<T extends Comparable<T>> {
+    /* ¡COMPLETAR! */
+
+
+    private class Nodo {
+        /* ¡COMPLETAR! */
+    }
+
+    public class HandleABB {
+        /* ¡COMPLETAR! */
+    }
+
+    public ABB() {
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+    public T minimo(){
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+    public HandleABB insertar(T elem){
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+    public boolean pertenece(T elem){
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+    public void eliminar(T elem){
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+    @Override
+    public String toString(){
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+    public class ABB_Iterador {
+        /* ¡COMPLETAR! */
+    }
+
+    public ABB_Iterador iterador() {
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+}
---
+++ taller5/src/main/java/aed/SistemaPedidos.java
@@ -0,0 +1,29 @@
+package aed;
+
+public class SistemaPedidos {
+    /* COMPLETAR atributos privados: ¿cómo se representan los pedidos? */
+
+    public SistemaPedidos(){
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+    public void agregarPedido(Pedido pedido){
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+    public Pedido proximoPedido(){
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+    public Pedido pedidoMenorId(){
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+    public String obtenerPedidosEnOrdenDeLlegada(){
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+
+    public String obtenerPedidosOrdenadosPorId(){
+        throw new UnsupportedOperationException("No implementado aún");
+    }
+}
